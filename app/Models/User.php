08d596d332a9ccd687bd54a0f1829b3c1a5f4159
dedyzhang <dedyzhang14@gmail.com>
2025-08-12 09:15:38 +0700

    #12 Agustus 2025 - Perubahan

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,6 +15,7 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, HasUuids;
     protected $table = 'users';
+    protected $keyType = 'string';
     protected $primaryKey = 'uuid';
 
     /**
